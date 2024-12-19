213da5d0dccef263510f86a9a398d146271cde6d
dhivakar020 <dhivakars2704@gmail.com>
2024-12-19 12:05:31 +0530

    secure commit

--- django_jwt_auth/settings.py
+++ django_jwt_auth/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zp9^rf-g^tcno6ejyb70a8wt&bot2(b09i866tzgtq!v&_s-4y'
+from decouple import config
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 ALLOWED_HOSTS = []
 
